42bc43243f051ed428342fa1d2d45c43e1b534f7
joao <joaovictoralvesbittencourt@gmail.com>
2025-05-03 17:41:05 -0300

    adicionando um record e fazendo um construtor de titulo para ele converter como exemplo titulo = title

--- src/br/com/alura/screenmatch/principal/MainSearch.java
+++ src/br/com/alura/screenmatch/principal/MainSearch.java
@@ -2,7 +2,10 @@ package br.com.alura.screenmatch.principal;
 
 
 import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
@@ -30,9 +33,13 @@ public class MainSearch {
         String json = response.body();
         System.out.println(json);
 
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
 
-        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
-        System.out.println("titulo: " + meuTitulo);
+        // Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
+        System.out.println("nome do filme: " + meuTituloOmdb );
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        System.out.println(meuTitulo);
     }
 }
