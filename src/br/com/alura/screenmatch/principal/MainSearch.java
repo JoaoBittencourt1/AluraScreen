25b54895fe4e9b308264bb0ee6d7181fd16742bc
joao <joaovictoralvesbittencourt@gmail.com>
2025-05-03 20:27:36 -0300

    adicionando o FileWriter para criar um arquivo txt com os dados obtidos da consultada da API sobre algum filme

--- src/br/com/alura/screenmatch/principal/MainSearch.java
+++ src/br/com/alura/screenmatch/principal/MainSearch.java
@@ -8,6 +8,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -43,6 +44,9 @@ public class MainSearch {
             // try{
             Titulo meuTitulo = new Titulo(meuTituloOmdb);
             System.out.println(meuTitulo);
+            FileWriter escrita = new FileWriter("filmes.txt");
+            escrita.write(meuTitulo.toString());
+            escrita.close();
         }catch (NumberFormatException e){
             System.out.println("aconteceu um erro!");
             System.out.println(e.getMessage());
