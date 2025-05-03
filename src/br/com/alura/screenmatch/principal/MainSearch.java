24503033aeefbb8b231d4ce14bea5f3292710b4a
joao <joaovictoralvesbittencourt@gmail.com>
2025-05-03 16:41:19 -0300

    convertendo, adicionando o GSON adicionando o SerializedName e criando um to String

--- src/br/com/alura/screenmatch/principal/MainSearch.java
+++ src/br/com/alura/screenmatch/principal/MainSearch.java
@@ -1,21 +1,38 @@
 package br.com.alura.screenmatch.principal;
 
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("digite um filme para busca: ");
+        var busca = input.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=cf6b4f27";
+
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=cf6b4f27"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println("titulo: " + meuTitulo);
     }
 }
